a789b6a399b058042d2a7a0dbd16906c4f22f25a
eric345int <ealgarra@iesjulioantonio.cat>
2025-04-24 11:10:36 +0200

    Add files via upload

--- P6_RA1_T1_A1_nomedat.py
+++ P6_RA1_T1_A1_nomedat.py
@@ -1,3 +1,13 @@
+#Administració de Sistemes Informàtics en Xarxa
+# Autor:Eric
+# Data:24/4/2025
+
+# Versió:1
+
+#Descripció:amb aquest programa hi pots sapiguer la edat i el nom.
+
+#Especificacions d'entrada: un numero major que 0 sense decimals i un nom.
+
 print("Quants anys tens?")
 
 edat = input()
