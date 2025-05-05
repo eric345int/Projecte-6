10260007db436d88404b7e310b917ee5db1117bc
eric345int <ealgarra@iesjulioantonio.cat>
2025-05-05 09:58:03 +0200

    Add files via upload

---
+++ #Administració de Sistemes Informàtics e.py
@@ -0,0 +1,16 @@
+#Administració de Sistemes Informàtics en Xarxa
+# Autor:Eric
+# Data:30/4/2025
+
+# Versió:9
+
+#Descripció:
+
+#Especificacions d'entrada:
+
+def suma(a, b):
+    return a + b
+
+print(suma(2, 3))
+print(suma(10, 20))
+print(suma(-5, 7))
---
+++ #Administració de Sistemes Informàtics e12.py
@@ -0,0 +1,16 @@
+#Administració de Sistemes Informàtics en Xarxa
+# Autor:Eric
+# Data:30/4/2025
+
+# Versió:9
+
+#Descripció:
+
+#Especificacions d'entrada: 
+
+def saluda():
+    print("Bon dia!")
+
+saluda()
+saluda()
+saluda()
