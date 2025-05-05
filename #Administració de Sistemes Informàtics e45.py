fe1dabb122ca261b86af2c0233a43abb10dc2ef8
eric345int <ealgarra@iesjulioantonio.cat>
2025-05-05 10:02:30 +0200

    Add files via upload

---
+++ #Administració de Sistemes Informàtics e45.py
@@ -0,0 +1,16 @@
+#Administració de Sistemes Informàtics en Xarxa
+# Autor:Eric
+# Data:30/4/2025
+
+# Versió:9
+
+#Descripció:procediment per a fer una suma.
+
+#Especificacions d'entrada:aixo hi serveix per a sumar diferents numeros.
+
+def suma(a, b):
+    return a + b
+
+print(suma(2, 3))
+print(suma(10, 20))
+print(suma(-5, 7))
